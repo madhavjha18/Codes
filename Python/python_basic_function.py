052242b94079e179668096b42ba457705a85763f
madhavjha18 <madhavjha18557@gmail.com>
2025-07-17 21:53:16 +0530

    changed

--- Python/python_basic_function.py
+++ Python/python_basic_function.py
@@ -7,7 +7,7 @@ def valid_age():
 
 print(valid_age())
 
-#built in method(function)
+#built in method(function)  
 name="madhav"
 print(name.upper()) #MADHAV
 print(name.lower()) #madhav
@@ -31,6 +31,20 @@ startsswith() to check if the string starts with a specific substring
 
 
 endswith() to check if the string ends with a specific substring
+
+isalpha() to check if a string contains only characters and is not empty
+
+isalnum() to check if a string contains characters or digits and is not empty, false when string contains space.
+
+isdecimal() to check if a string contains only digits and is not empty
+
+len() to get integer length of string,list,tuple,dictionary
+
+type() to get the type of object
+
+abs() to get positive integer of any no. eg. abs(-5)=5
+
+
 
 """
 #replace() to replace a part of a string
@@ -70,7 +84,91 @@ position = message.find("needle")
 print(position)  # Output: 9 (index where "needle" starts)
 
 
+#The any() function in Python returns True if at least one element in an iterable is truthy, and False if all elements are falsy or the iterable is empty.
+print(any([False, False, True]))  # True
+print(any([0, False, "", None]))  # False
+
+
+#The all() function in Python returns True if all elements in an iterable are truthy, and False if any element is falsy or the iterable is empty.
+print(all([True, 1, "text"]))  # True
+print(all([]))  # True (by definition – vacuously true)
+print(all([True, 0, "hello"]))  # False (0 is falsy)
+
+
+#complex numbers
+num1 = 2+3j
+num2 = complex(2,3)
+print(num2.real, num2.imag)
+
+
+#round function
+num3= 5.23
+num4=5.65
+print(round(num3))
+print(round(num4,1)) #rounds to 1 decimal point
+
+"""
+remove(x) Removes the first occurrence of the element x from the list.
+
+append(x) Adds x to the end of the list.
+
+pop([i]) Removes and returns the element at index i. If no index is given, removes the last item.
+
+insert(i, x) Inserts x at index i, shifting elements to the right.
+
+
+"""
+
+# extend(iterable) Adds all elements from another iterable (e.g. list) to the end.
+lst = [1, 2]
+lst.extend([3, 4])
+print(lst)  # [1, 2, 3, 4]
 
 
 
+#sort() to sort a list (it should have same data type)
+numbers = [3, 1, 4, 2]
+numbers.sort()
+print(numbers)  # [1, 2, 3, 4]
 
+numbers.sort(reverse=True)
+print(numbers)  # [4, 3, 2, 1]
+
+words = ["banana", "apple", "cherry"]
+words.sort(key=len)  # Sort by length
+print(words)  # ['apple', 'banana', 'cherry']
+
+fruit=["Apple","cherry","mango","Banana"]
+fruit.sort(key=str.lower) #sort without 
+print(fruit) #["Apple","Banana","cherry","mango"]
+
+#using sorted() keyword Original stays intact
+a = [3, 1, 2]
+print(a)  # [3, 1, 2]
+print(sorted(a))  # [1, 2, 3]
+
+
+
+#The index() method in Python returns the index of the first occurrence of a specified value in a list.
+fruits = ['apple', 'banana', 'cherry', 'banana']
+i = fruits.index('banana')
+print(i)  # 1
+
+
+
+#split() Splits a String into a List
+text = "apple banana cherry"
+parts = text.split()  # default: split by space
+print(parts)  # ['apple', 'banana', 'cherry']
+
+texts = "name,email,age"
+parts = texts.split(",")
+print(parts)  # ['name', 'email', 'age']
+
+#join() Joins a List into a String
+words = ['apple', 'banana', 'cherry']
+text = " ".join(words)
+print(text)  # "apple banana cherry"
+
+csv = ",".join(['name', 'email', 'age'])
+print(csv)  # "name,email,age"
