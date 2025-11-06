3ffef6e6322bfb65868be22ae3c9f8e56bc48382
madhavjha18 <madhavjha18557@gmail.com>
2025-11-07 00:06:32 +0530

    new

---
+++ C++/perfect_sum_prob.cpp
@@ -0,0 +1,19 @@
+// This code is to calculate number of combination of subsequence to get target sum
+
+#include "my_std.h"
+
+int perfectsum(vector<int> &arr, int sum, int index){
+    if(index == arr.size()){
+        return sum == 0;
+    }
+
+    return perfectsum(arr,sum,index+1) + perfectsum(arr,sum - arr[index],index+1); // add sum+arr[index] for negative 
+}
+
+int main(){
+    vector<int>arr = {1,0,0,1};
+
+    int target = 2;
+
+    cout<<perfectsum(arr,target,0);
+}
