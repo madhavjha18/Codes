32e1b436f5cfd02eb44fcf29a4208da4ccaab548
madhavjha18 <madhavjha18557@gmail.com>
2025-11-08 16:36:57 +0530

    new

---
+++ C++/permutation_swap.cpp
@@ -0,0 +1,29 @@
+#include "my_std.h"
+
+void permutation(vector<int> &arr, vector<vector<int>>& ans,int index){
+    if(index == arr.size()){
+        ans.push_back(arr);
+        return;
+    }
+
+    for(int i=index; i<arr.size();i++){
+        swap(arr[i],arr[index]);
+        permutation(arr,ans,index+1);
+        swap(arr[index],arr[i]);
+    }
+}
+int main(){
+    vector<int>arr = {1,2,3,4,5,6};
+    vector<vector<int>>ans;
+
+    permutation(arr,ans,0);
+
+    for(auto &row : ans){
+        for(auto &i : row){
+            cout<<i<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<ans.size();
+    return 0;
+}
