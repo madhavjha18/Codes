3d1e36e3780c685134ce7f93672d2aa9e98ca788
madhavjha18 <madhavjha18557@gmail.com>
2025-11-24 22:03:42 +0530

    pallindrome

---
+++ C++/print_N_pallindrome.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+ bool pallindrome(string& s,int left,int right){
+     if(left>=right) return true;
+     
+     if(s[left] != s[right]) return false;
+     
+     return pallindrome(s,left+1,right-1);
+ }
+int main() {
+    // printing 0-n pallindrome
+    
+    int n;
+    cout<<"Enter value of N which is the upper limit: ";
+    cin>>n;
+    
+    vector<int>ans;
+    for(int i = 0;i<=n;i++){
+        string s = to_string(i);
+        if(pallindrome(s,0,s.length()-1)){
+            ans.push_back(i);
+        }
+    }
+    for(auto &i : ans) cout<<i<<" ";
+
+	return 0;
+}
