bf00c839b0dd018c849f75810d4f9f6b06126977
madhavjha18 <madhavjha18557@gmail.com>
2026-04-10 18:38:13 +0530

    heapsort

--- C++/HeapSort.cpp
+++ C++/HeapSort.cpp
@@ -1,26 +1,65 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-// Step Up Approach
-void heapify(vector<int>&arr,int n){
+// // Step up Approach (time complexity : nlogn)
+// void heapify(vector<int>&arr,int n){
 
-    for(int i = 1;i<n;i++){
-        int curr = i;
+//     for(int i = 1;i<n;i++){
+//         int curr = i;
 
-        while(curr > 0 && arr[curr] > arr[(curr - 1)/2]){
-            swap(arr[curr],arr[(curr - 1)/2]);
-            curr = (curr - 1)/2;
-        }
+//         while(curr > 0 && arr[curr] > arr[(curr - 1)/2]){
+//             swap(arr[curr],arr[(curr - 1)/2]);
+//             curr = (curr - 1)/2;
+//         }
+//     }
+// }
+
+// // heapsort function for step up
+// void heapsort(vector<int>&arr){
+//     int n = arr.size();
+
+//     for(int i = n - 1;i>0;i--){
+//         swap(arr[0],arr[i]);
+//         heapify(arr,i);
+//     }
+// }
+
+//step down approach (time complexity: n)
+void heapify(vector<int>&arr,int n,int index){
+    int largest = index;
+    int left = 2*index + 1;
+    int right = 2*index + 2;
+
+    if(left < n && arr[left] > arr[index])
+    largest = left;
+    if(right < n && arr[right] > arr[index])
+    largest = right;
+
+    if(largest != index){
+        swap(arr[largest],arr[index]);
+        heapify(arr,n,largest);
     }
 }
 
+// heapsort function for step down approach
 void heapsort(vector<int>&arr){
     int n = arr.size();
 
     for(int i = n - 1;i>0;i--){
         swap(arr[0],arr[i]);
-        heapify(arr,i);
+        heapify(arr,i,0);
     }
+}
+
+//build initial heap then sort
+void buildheap(vector<int>&arr){
+    int n = arr.size();
+
+    for(int i = n/2 - 1;i>=0;i--)
+    heapify(arr,n,i);
+
+    //sort the array
+    heapsort(arr);
 }
 
 void print(vector<int>&arr){
@@ -31,12 +70,11 @@ void print(vector<int>&arr){
 }
 int main() {
 
-    vector<int>arr = {12,25,63,73,78,33,84,85,96,23,74,11,43,65,85,82,83,49};
+    vector<int>arr = {12,25,63,73,78,33,84,85,96,23,74,11,43,65,85,82,83,49,99};
 
     int n = arr.size();
-    heapify(arr,n);
 
-    heapsort(arr);
+    buildheap(arr);
 
     print(arr);
 
